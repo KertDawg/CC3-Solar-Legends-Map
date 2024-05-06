c969eed5c3360fbd94aa4eed635ecc4279add7f9
Kertis Henderson <kertishenderson@gmail.com>
2024-05-06 13:26:19 -0400

    Changed starting theta to look better in early days

--- CC3PlanetXP/Planets.cpp
+++ CC3PlanetXP/Planets.cpp
@@ -42,12 +42,12 @@ Planets::Planets(int ClosestPlanet, int FarthestPlanet, int SystemCurrentDay, fl
 			break;
 		case 2:
 			OnePlanet = new Planet();
-			OnePlanet->Set("Venus", 0.73f, 224.7f, 20, 0.9f, CurrentDay, ReloativeSize, "Venus");
+			OnePlanet->Set("Venus", 0.73f, 224.7f, 60, 0.9f, CurrentDay, ReloativeSize, "Venus");
 			ListOfPlanets.push_back(*OnePlanet);
 			break;
 		case 4:
 			OnePlanet = new Planet();
-			OnePlanet->Set("Mars", 1.67f, 686.98f, 30, 1, CurrentDay, ReloativeSize, "Mars");
+			OnePlanet->Set("Mars", 1.67f, 686.98f, 230, 1, CurrentDay, ReloativeSize, "Mars");
 			ListOfPlanets.push_back(*OnePlanet);
 			break;
 		case 5:
@@ -57,28 +57,28 @@ Planets::Planets(int ClosestPlanet, int FarthestPlanet, int SystemCurrentDay, fl
 			break;
 		case 6:
 			OnePlanet = new Planet();
-			OnePlanet->Set("Saturn", 10.07f, 29.46f * 365.26f, 50, 4, CurrentDay, ReloativeSize, "Saturn");
+			OnePlanet->Set("Saturn", 10.07f, 29.46f * 365.26f, 300, 4, CurrentDay, ReloativeSize, "Saturn");
 			ListOfPlanets.push_back(*OnePlanet);
 			break;
 		case 7:
 			OnePlanet = new Planet();
-			OnePlanet->Set("Uranus", 20.09f, 84.01f * 365.26f, 60, 1, CurrentDay, ReloativeSize, "Uranus");
+			OnePlanet->Set("Uranus", 20.09f, 84.01f * 365.26f, 120, 1, CurrentDay, ReloativeSize, "Uranus");
 			ListOfPlanets.push_back(*OnePlanet);
 			break;
 		case 8:
 			OnePlanet = new Planet();
-			OnePlanet->Set("Neptune", 164.79f * 365.26f, 30.32f, 70, 1, CurrentDay, ReloativeSize, "Neptune");
+			OnePlanet->Set("Neptune", 164.79f * 365.26f, 30.32f, 180, 1, CurrentDay, ReloativeSize, "Neptune");
 			ListOfPlanets.push_back(*OnePlanet);
 			break;
 		case 9:
 			OnePlanet = new Planet();
-			OnePlanet->Set("Pluto", 90650.0f, 48.823f, 80, 0.5, CurrentDay, ReloativeSize, "Mercury");
+			OnePlanet->Set("Pluto", 90650.0f, 48.823f, 250, 0.5, CurrentDay, ReloativeSize, "Mercury");
 			ListOfPlanets.push_back(*OnePlanet);
 			break;
 
 		default:
 			OnePlanet = new Planet();
-			OnePlanet->Set("Earth", 1.0f, 1.0f, 90, 1, CurrentDay, ReloativeSize, "Earth");
+			OnePlanet->Set("Earth", 1.0f, 1.0f, 160, 1, CurrentDay, ReloativeSize, "Earth");
 			ListOfPlanets.push_back(*OnePlanet);
 		}
 	}
